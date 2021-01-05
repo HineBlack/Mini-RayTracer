5f0c2fe5635fa59a2f5f61197226cfba5ee4f1a0
Hine <jxaa121678@gmail.com>
2021-01-05 20:41:20 +0800

    display the normal of sphere

--- src/main.cc
+++ src/main.cc
@@ -3,19 +3,26 @@
 #include "ray.h"
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    auto oc = r.origin() - center;
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
-    return ((b*b - 4*a*c) >= 0);
+    auto discriminant = b*b - 4*a*c;
+    if (discriminant >= 0){
+        return (-b - sqrt(discriminant) ) / (2.0*a);
+    }
+    return -1.0;
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1,0,0);
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector( r.at(t) - vec3(0,0,-1) );
+        return N*0.5 + vec3(0.5, 0.5, 0.5);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
