364578663c6029dfe4bb1cb9e086cdb6285a50ce
Hine <jxaa121678@gmail.com>
2021-01-08 23:35:57 +0800

    schlick approximation fixed

--- src/camera.h
+++ src/camera.h
@@ -5,6 +5,9 @@
 class camera {
     public:
         camera(
+            point3 lookfrom,
+            point3 lookat,
+            vec3   vup,
             double vfov,
             double aspect_ratio
         ) {
@@ -13,21 +16,23 @@ class camera {
             auto viewport_height = 2.0 * h;
             auto viewport_width = aspect_ratio * viewport_height;
 
-            auto focal_length = 1.0;
+            auto w = unit_vector(lookfrom - lookat);
+            auto u = unit_vector(cross(vup, w));
+            auto v = cross(w, u);
 
-            origin = point3(0,0,0);
-            horizontal = vec3(viewport_width, 0.0, 0.0);
-            vertical = vec3(0.0, viewport_height, 0.0);
+            origin = lookfrom;
+            horizontal = viewport_width * u;
+            vertical = viewport_height * v;
             lower_left_corner = origin
                                 - 0.5*horizontal
                                 - 0.5*vertical
-                                - vec3(0, 0, focal_length);
+                                - w;
         }
 
-        ray get_ray(double u, double v) const {
+        ray get_ray(double s, double t) const {
             return ray(origin, lower_left_corner
-                                + u*horizontal
-                                + v*vertical
+                                + s*horizontal
+                                + t*vertical
                                 - origin);
         }
 
